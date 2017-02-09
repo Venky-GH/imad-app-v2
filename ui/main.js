6d235c9a6367de7bc3b75db66fa65183368ac139
Venky-GH <venkynaidu1997@gmail.com>
2017-02-09 17:57:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,12 +2,19 @@ console.log('Loaded!');
 
 var counter= 0;
 
-var a = document.getElementById("button");
+var request = new XMLHttpRequest();
 
-a.onclick = function(){
-  
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+request.onreadystatechange = function(){
+  if(request.readyState === readyState.DONE)
+  {
+      if(request.status === 200)
+      {
+          counter = request.responseText;
+          var span = document.getElementById('count');
+          span.innerHTML = counter.toString();
+      }
+  }
 };
 
+request.open('GET','http://venky-gh.imad.hasura-app.io/',true);
+request.send(null);
