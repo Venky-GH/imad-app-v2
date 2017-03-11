eebe861982c9fb0bd20831e0182f2b91bdb58a21
Venky-GH <venkynaidu1997@gmail.com>
2017-03-11 10:09:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,8 +101,21 @@ app.get('/', function (req, res) {
 
 
 app.get('/:articleName',function (req,res){
-    var articleName = req.params.articleName;
-    res.send(aa(articles[articleName])); 
+    pool.query('SELECT * FROM article WHERE title = ' + req.params.articleName, function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } 
+       else{
+           if(result.rows.length === 0){
+               res.status(404).send('No Such Article Found!');
+           }
+           else{
+               var articleData = result.rows[0];
+               res.send(aa(articleData));
+           }
+       }
+    });
+    
 });
 
 
