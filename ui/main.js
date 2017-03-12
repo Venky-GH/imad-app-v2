4e4ba474206045c0f23e27e8a6bf55781ae26b27
Venky-GH <venkynaidu1997@gmail.com>
2017-03-12 20:00:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,6 +28,7 @@ request.onreadystatechange = function(){
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 request.open('POST','http://venky-gh.imad.hasura-app.io/login' ,true);
+request.setRequestHeader('Content-type','application/json');    
 request.send(JSON.stringify({username: username, password: password}));
 
 };
