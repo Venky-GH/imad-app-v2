ead661425525e044023090498ba864c53169267e
Venky-GH <venkynaidu1997@gmail.com>
2017-03-12 19:05:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,32 @@ var button = document.getElementById('b');
 
 var but = document.getElementById('button');
 
+var sub = document.getElementById('Submit');
+
+sub.onclick = function (){
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status === 200)
+      {
+          alert('Successfully logged in!');
+      }
+      else if(request.status === 403){
+          alert('Invalid Username/Password!');
+      }
+      else if(request.status === 500){
+          alert('Something went wrong on the server!');
+      }
+  }
+};
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+request.open('POST','http://venky-gh.imad.hasura-app.io/login' ,true);
+request.send(JSON.stringify({username: username, password: password}));
+
+};
 
 but.onclick = function (){
 var request = new XMLHttpRequest();
@@ -34,6 +60,7 @@ request.send(null);
 
 };
 
+/*
 button.onclick = function(){
   
   var request = new XMLHttpRequest();
@@ -54,5 +81,6 @@ button.onclick = function(){
     }
   };
 };
+*/
 
 
