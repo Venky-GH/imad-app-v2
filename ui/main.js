1a8b6578c613b45887546fb296e43b877c2741ee
Venky-GH <venkynaidu1997@gmail.com>
2017-03-27 15:50:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,33 @@ button.onclick = function(){
     document.getElementById("naidu").innerHTML = counter.toString();
 };
 
+but.onclick = function (){
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+  if(request.readyState === XMLHttpRequest.DONE)
+  {
+      if(request.status === 200)
+      {
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list = '';
+          
+          for (var i=0; i<names.length; i++)
+          {
+              list += '<li>' + names[i] + '</li>';
+          }
+          var ul = document.getElementById("ullist");
+          ul.innerHTML = list;
+      }
+  }
+};
+var name_1 = document.getElementById('n');
+var name = name_1.value;
+request.open('GET','http://venky-gh.imad.hasura-app.io/submit?name=' + name ,true);
+request.send(null);
+
+};
 
 su.onclick = function (){
 var request = new XMLHttpRequest();
@@ -80,33 +107,7 @@ redi.onclick = function(){
 };
 
 
-but.onclick = function (){
-var request = new XMLHttpRequest();
 
-request.onreadystatechange = function(){
-  if(request.readyState === XMLHttpRequest.DONE)
-  {
-      if(request.status === 200)
-      {
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-          
-          for (var i=0; i<names.length; i++)
-          {
-              list += '<li>' + names[i] + '</li>';
-          }
-          var ul = document.getElementById("ullist");
-          ul.innerHTML = list;
-      }
-  }
-};
-var name_1 = document.getElementById('n');
-var name = name_1.value;
-request.open('GET','http://venky-gh.imad.hasura-app.io/submit?name=' + name ,true);
-request.send(null);
-
-};
 
 
 
