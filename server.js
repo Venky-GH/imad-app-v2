ede3bf35ec069a3d02f8d27b2899aaebc833cf28
Venky-GH <venkynaidu1997@gmail.com>
2017-03-27 20:02:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -208,6 +208,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/main1.js', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'main1.js')); 
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
