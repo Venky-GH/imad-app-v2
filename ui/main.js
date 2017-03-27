4a2ba73fc9b2b59b87bff9eac2b31730a8062e89
Venky-GH <venkynaidu1997@gmail.com>
2017-03-27 19:59:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,7 @@ var button = document.getElementById('venky');
 
 var but = document.getElementById('button');
 
-var su = document.getElementById('sub');
-var r = document.getElementById('re');
+
 
 button.onclick = function(){
     var request = new XMLHttpRequest();
@@ -52,64 +51,6 @@ request.send(null);
 
 };
 
-su.onclick = function (){
-var request = new XMLHttpRequest();
-
-request.onreadystatechange = function(){
-  if(request.readyState === XMLHttpRequest.DONE)
-  {
-      if(request.status === 200)
-      {
-          alert('Successfully logged in!');
-          console.log('Login Successful!');
-          location.href = "http://venky-gh.imad.hasura-app.io/check-login";
-      }
-      else if(request.status === 403){
-          alert('Invalid Username/Password!');
-      }
-      else if(request.status === 500){
-          alert('Something went wrong on the server!');
-      }
-  }
-};
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST','http://venky-gh.imad.hasura-app.io/login' ,true);
-request.setRequestHeader('Content-type','application/json');    
-request.send(JSON.stringify({username: username, password: password}));
-
-};
-
-r.onclick = function (){
-var request = new XMLHttpRequest();
-
-request.onreadystatechange = function(){
-  if(request.readyState === XMLHttpRequest.DONE)
-  {
-      if(request.status === 200)
-      {
-          alert('Successfully Registered!');
-          location.href = "http://venky-gh.imad.hasura-app.io/user";
-      }
-      else if(request.status === 403){
-          alert('Username already used!');
-      }
-      else if(request.status === 500){
-          alert('Something went wrong on the server!');
-      }
-  }
-};
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST','http://venky-gh.imad.hasura-app.io/create-user' ,true);
-request.setRequestHeader('Content-type','application/json');    
-request.send(JSON.stringify({username: username, password: password}));
-
-};
 
 
 
