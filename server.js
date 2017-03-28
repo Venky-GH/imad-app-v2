c7a9b81ebd84d17a4a0d71cb1f3d1ca82416c774
Venky-GH <venkynaidu1997@gmail.com>
2017-03-28 17:53:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -216,7 +216,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-
+app.get('/ui/batman-logo-wallpaper-for-desktop-1080p-123.jpg', function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'batman-logo-wallpaper-for-desktop-1080p-123.jpg')); 
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
