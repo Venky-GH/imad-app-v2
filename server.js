3362a42a60ab1998d166f2527725512f653d72ad
Venky-GH <venkynaidu1997@gmail.com>
2017-03-29 23:43:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,12 +155,14 @@ var aap = `<html>
     <link href="/ui/style.css" rel="stylesheet" />
     </head>
     <body>
+      <div>
         <div class="abc">
             <h1 class="heading"><a class="href" href="/">${home}</a></h1>
         </div>
-        <div class="abc">
+        <div>
         ${content}
         </div>
+      </div>
     </body>
 </html>
 `;
