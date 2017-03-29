29135af36c3a89b350488ce4314b7d13641412ac
Venky-GH <venkynaidu1997@gmail.com>
2017-03-29 15:48:30 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -2,3 +2,23 @@ var f = document.getElementById('form');
 f.onclick = function(){
   location.href = "http://venky-gh.imad.hasura-app.io/";
 };
+
+var t = document.getElementById('hsubmit');
+t.onclick = function(){
+  
+  var request = new XMLHttpRequest();
+  
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var h = document.getElementById('houtput');
+            var k = request.responseText;
+            h.value = k.toString();
+        }
+    }  
+  };
+  var g = document.getElementById('hinput');
+  var y = g.value;
+  request.open('GET','http://venky-gh.imad.hasura-app.io/hash/:' + y.true);
+  request.send(null);
+};
